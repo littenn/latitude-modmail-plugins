856dd5a12c97da365334e006e53071b71d588da8
littenn <69858877+littenn@users.noreply.github.com>
2021-03-13 20:57:30 -0500

    Update HELLO.js

--- .github/workflows/plugins/HELLO.js
+++ .github/workflows/plugins/HELLO.js
@@ -1,7 +1,4 @@
-client.on('message', message => {
-	if (message.content === 'hello') {
-		message.channel.send('hello!');
-	}
-});
+channel = client.get_channel(698344393249128550)
+await channel.send('hello')
 
 // now fuck off with stealing code, deffo didn't take 1 second to make :(!!
